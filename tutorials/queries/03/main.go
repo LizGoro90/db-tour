9b7b0db9e5db159b43d3334f513e5319e113f062
Lizbeth Gómez <lizgoro@la-MacBook-Air-de-Lizbeth.local>
2018-07-16 12:59:14 -0500

    Style correction (summarized ideas and emphasized division between the success and failure cases)

--- tutorials/queries/03/main.go
+++ tutorials/queries/03/main.go
@@ -42,10 +42,10 @@ func main() {
 		log.Printf("Book %d:\t%#v", book.ID, book)
 	}
 
-	// In case anything bad happens, Next will break the loop and generate an
-	// error, you can retrieve that error by calling Err. Err should be nil at
-	// the end of any succesfully completed loop (even if the dataset had no
-	// elements).
+	// In the event of a problem, Next will break the loop and generate an
+	// error (which can be retrieved by calling Err). On the other hand, when
+	// the loop is successfully completed (even if the data set had no items),
+	// Err will be nil.
 	if err := res.Err(); err != nil {
 		log.Printf("Next exited with error: %v.", err)
 		log.Printf(`SUGGESTION: Change OrderBy("-xid") into OrderBy("id") on the result set definition and try again.`)
